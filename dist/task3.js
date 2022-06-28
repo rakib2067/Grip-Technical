10a8571be1a94d375b86ca72003f53b3d1ea7844
rakib2067 <rakib7@hotmail.co.uk>
2022-06-28 14:59:20 +0100

    changed task 4 function

--- dist/task3.js
+++ dist/task3.js
@@ -12,32 +12,23 @@ function getUsers(records, action, start_time, end_time) {
 function getPlaybackTime(user_id, records) {
     let totalTime = 0;
     const filteredArray = records.filter(record => record.user_id === user_id);
-    let overlap = false;
     let overlapStopTime = 0;
     for (let i = 0; i < filteredArray.length; i++) {
         let currentRecord = filteredArray[i];
         let currentDevice = currentRecord.device;
-        for (let j = i + 1; j < filteredArray.length; j++) {
-            let testRecord = filteredArray[j];
-            if (testRecord.device !== currentDevice && testRecord.action === "start") {
-                console.log(`There has been an overlap between ${currentDevice} and ${testRecord}  Overlap:${overlap} will be set to true`);
-                overlap = true;
-            }
-            else if (testRecord.device === currentDevice && !overlap) {
-                totalTime = totalTime + (testRecord.date_actioned - currentRecord.date_actioned);
-                overlapStopTime = testRecord.date_actioned;
-                overlap = false;
-                break;
-            }
-            else if (testRecord.device === currentDevice && overlap && overlapStopTime == 0) {
-                totalTime = totalTime + (testRecord.date_actioned - currentRecord.date_actioned);
-                overlapStopTime = testRecord.date_actioned;
-                break;
-            }
-            else if (testRecord.device === currentDevice && overlap && overlapStopTime !== 0) {
-                totalTime = totalTime + (testRecord.date_actioned - overlapStopTime);
-                overlap = false;
-                break;
+        if (currentRecord.action === "start") {
+            for (let j = i + 1; j < filteredArray.length; j++) {
+                let testRecord = filteredArray[j];
+                if (testRecord.device == currentDevice && testRecord.action === "stop" && currentRecord.date_actioned > overlapStopTime) {
+                    totalTime = totalTime + (testRecord.date_actioned - currentRecord.date_actioned);
+                    overlapStopTime = testRecord.date_actioned;
+                    break;
+                }
+                else if (testRecord.device == currentDevice && testRecord.action === "stop" && overlapStopTime < testRecord.date_actioned) {
+                    totalTime = totalTime + (testRecord.date_actioned - overlapStopTime);
+                    overlapStopTime = testRecord.date_actioned;
+                    break;
+                }
             }
         }
     }
@@ -56,4 +47,28 @@ let res = getUsers(records, 'start', 700, 900);
 console.log(res);
 let res2 = getPlaybackTime(1, records);
 console.log(res2);
+const records2 = [
+    { user_id: 1, device: "Windows 10", action: "start", date_actioned: 100 },
+    { user_id: 1, device: "OSX 15.4", action: "start", date_actioned: 200 },
+    { user_id: 1, device: "iPhone 8s", action: "start", date_actioned: 250 },
+    { user_id: 1, device: "iPhone 8s", action: "stop", date_actioned: 370 },
+    { user_id: 1, device: "Windows 10", action: "stop", date_actioned: 410 },
+    { user_id: 1, device: "Android", action: "start", date_actioned: 450 },
+    { user_id: 1, device: "OSX 15.4", action: "stop", date_actioned: 490 },
+    { user_id: 1, device: "Android", action: "stop", date_actioned: 500 },
+];
+let res3 = getPlaybackTime(1, records2);
+console.log(res3);
+const records3 = [
+    { user_id: 1, device: "Windows 10", action: "start", date_actioned: 100 },
+    { user_id: 1, device: "Windows 10", action: "stop", date_actioned: 200 },
+    { user_id: 1, device: "OSX 15.4", action: "start", date_actioned: 250 },
+    { user_id: 1, device: "OSX 15.4", action: "stop", date_actioned: 300 },
+    { user_id: 1, device: "iPhone 8s", action: "start", date_actioned: 350 },
+    { user_id: 1, device: "iPhone 8s", action: "stop", date_actioned: 400 },
+    { user_id: 1, device: "Android", action: "start", date_actioned: 450 },
+    { user_id: 1, device: "Android", action: "stop", date_actioned: 500 },
+];
+let res4 = getPlaybackTime(1, records3);
+console.log(res4);
 //# sourceMappingURL=task3.js.map
--- src/task3.ts
+++ src/task3.ts
@@ -31,48 +31,41 @@ function getPlaybackTime(user_id:number,records:UserRecord [] ):number{
     
 
     //Variables declared outside of the scope of loops, to check for overlapping, and store date_actioned of previous stop time
-    let overlap=false
-
     let overlapStopTime: number= 0;
+
     for(let i=0;i<filteredArray.length;i++){
         
         let currentRecord=filteredArray[i]
         let currentDevice= currentRecord.device
+        if( currentRecord.action==="start"){
 
+        //Looping over starting from next index
         for(let j=i+1; j<filteredArray.length;j++){
 
-            //variables for test record
+            //variable for test record
             let testRecord=filteredArray[j]
 
 
-            if(testRecord.device!==currentDevice && testRecord.action==="start"){
-                console.log(`There has been an overlap between ${currentDevice} and ${testRecord}  Overlap:${overlap} will be set to true`)
-                overlap=true
-            }
-            else if(testRecord.device===currentDevice && !overlap){
-                //If there is no overlap, add whole duration to total time
-                totalTime=totalTime+ (testRecord.date_actioned-currentRecord.date_actioned)
-                overlapStopTime=testRecord.date_actioned
-                overlap=false
-                break
-            }
-            else if(testRecord.device===currentDevice && overlap && overlapStopTime==0){
-                // If we are testing our first record and an overlap has been detected
+            if(testRecord.device == currentDevice && testRecord.action==="stop"&& currentRecord.date_actioned>overlapStopTime){
+                //Total duration for for record or if there is no overlap on subsequent devices
                 totalTime=totalTime+ (testRecord.date_actioned-currentRecord.date_actioned)
                 overlapStopTime=testRecord.date_actioned
                 break
             }
-            else if(testRecord.device===currentDevice && overlap && overlapStopTime!==0){
-                // Handling overlaps from records after the first
-                // subtract current stop from previous stop to get the unique duration
+            else if(testRecord.device == currentDevice &&testRecord.action==="stop"&& overlapStopTime<testRecord.date_actioned){
+
+                //If the testrecord device has a stop time higher than our current one (It is overlapping)
+                // add to total time, using difference of the current stop time
                 totalTime=totalTime+ (testRecord.date_actioned- overlapStopTime)
-                overlap=false
+
+                //update stop time
+                overlapStopTime=testRecord.date_actioned;
                 break
             }
-        }
+            //For cases where it is overlapping within our stoptime, nothing happens
+        }}
     }
     return totalTime
-
 }
 
 const records: UserRecord[]=[
@@ -85,8 +78,44 @@ const records: UserRecord[]=[
     { user_id: 3, device: "Android 9.1", action: "start", date_actioned: 700 },
 ]
 
+
+
+
+
 let res=getUsers(records,'start',700, 900)
 console.log(res)
 
 let res2=getPlaybackTime(1,records)
 console.log(res2)
+
+
+
+// tests for overlaps, and not adding overlapping playbacktime
+const records2: UserRecord[]=[
+    { user_id: 1, device: "Windows 10", action: "start", date_actioned: 100 },
+    { user_id: 1, device: "OSX 15.4", action: "start", date_actioned: 200 },
+    { user_id: 1, device: "iPhone 8s", action: "start", date_actioned: 250 },
+    { user_id: 1, device: "iPhone 8s", action: "stop", date_actioned: 370 },
+    { user_id: 1, device: "Windows 10", action: "stop", date_actioned: 410 },
+    { user_id: 1, device: "Android", action: "start", date_actioned: 450 },
+    { user_id: 1, device: "OSX 15.4", action: "stop", date_actioned: 490 },
+    { user_id: 1, device: "Android", action: "stop", date_actioned: 500 },
+
+]
+let res3=getPlaybackTime(1,records2)
+console.log(res3)
+
+// Test for no overlap
+const records3: UserRecord[]=[
+    { user_id: 1, device: "Windows 10", action: "start", date_actioned: 100 },
+    { user_id: 1, device: "Windows 10", action: "stop", date_actioned: 200 },
+    { user_id: 1, device: "OSX 15.4", action: "start", date_actioned: 250 },
+    { user_id: 1, device: "OSX 15.4", action: "stop", date_actioned: 300 },
+    { user_id: 1, device: "iPhone 8s", action: "start", date_actioned: 350 },
+    { user_id: 1, device: "iPhone 8s", action: "stop", date_actioned: 400 },
+    { user_id: 1, device: "Android", action: "start", date_actioned: 450 },
+    { user_id: 1, device: "Android", action: "stop", date_actioned: 500 },
+]
+
+let res4=getPlaybackTime(1,records3)
+console.log(res4)
